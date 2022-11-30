959753f6195efda995c8a2d13a9d0b9060adb22a
federossi93 <rossifederico0993@gmail.com>
2022-11-30 17:39:06 +0100

    add style

--- index.php
+++ index.php
@@ -11,15 +11,28 @@
     <title>Document</title>
 </head>
 
-<body>
+<body class="bg-primary">
     <div id="app">
-        <div class="container">
-        <h2>Todo List</h2>
-        <ul>
-            <li v-for="task in tasks">{{task}}</li>
-            
-        </ul>
+        <h2 class="text-center my-5">Todo List</h2>
+        <div class="container d-flex justify-content-center">
+            <ul class="list-group col-7">
+                <li v-for="task in tasks" class="list-group-item">{{task}}</li>
+            </ul>
         </div>
+
+        <div class=" container d-flex justify-content-center my-3">
+            <div class="col-7">
+                <form action="server.php" method="post">
+                <div class="input-group mb-3 ">
+                    <input type="text" class="form-control" placeholder="Recipient's username"
+                        aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <button class="btn btn-secondary" type="button" id="button-addon2">Button</button>
+            </form>
+            </div>
+            
+        </div>
+
+    </div>
     </div>
 
 
@@ -27,9 +40,13 @@
 
 
     <!--script axios-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.2.0/axios.min.js" integrity="sha512-OdkysyYNjK4CZHgB+dkw9xQp66hZ9TLqmS2vXaBrftfyJeduVhyy1cOfoxiKdi4/bfgpco6REu6Rb+V2oVIRWg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.2.0/axios.min.js"
+        integrity="sha512-OdkysyYNjK4CZHgB+dkw9xQp66hZ9TLqmS2vXaBrftfyJeduVhyy1cOfoxiKdi4/bfgpco6REu6Rb+V2oVIRWg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!--script bootstrap-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
     <!--script vue-->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
